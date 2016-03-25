5cf11976762b72ed61820fc5344429ea1e02cc4c
Carlos Suárez <smca310777@gmail.com>
2016-03-25 11:51:05 -0600

    Actividad3

---
+++ actividad3/TM_A_03.playground/Contents.swift
@@ -0,0 +1,72 @@
+import UIKit
+/*:
+# Playground - Actividad 3
+* Tipos de datos
+* Asociación de tipos
+* Arreglos y Diccionarios
+* Condicionales y Ciclos
+* Funciones
+* Enumareción
+*/
+
+
+/*: 
+### Actividad de Tipos de datos
+A) Declarar cuatro variables con tres diferentes tipos de datos, cada uno que corresponda a una numero entero, un numero decimal (flotante), una cadena de texto, realizando la asignación explicita y la asignación inferida
+*/
+
+
+
+
+/*:
+### Asociación de tipos
+A) Declara el tipo de dato por asociación para un tipo de dato String
+*/
+
+//: B) Declara el tipo de dato por asociación para un tipo de dato  Número entero.
+
+
+
+/*: 
+### Arreglos y Diccionarios
+A) Crea la variable "numeros" de tipo Array con números consecutivos del 1 a 10.
+*/
+
+//: B) Crea la variable "diasSemana" de tipo Dictionary con la relación numero:día Ej. 1:"Lunes"
+
+
+/*:
+### Condicionales y Ciclos
+A) Declarar la variable "datos" con los valores [3,6,9,2,4,1]
+*/
+
+//: B) realizar el recorrido de la variable "datos" con la instrucción "for"
+
+
+//: C) Encontrar los valores menores a 5
+
+
+/*:
+### Funciones
+A) Crea la función "suma" que reciba dos parámetros de tipo entero regresando la suma de ambos números.
+*/
+
+
+//: B) Crear la función "potencia" que reciba dos parámetros de tipo entero, el primer parámetro para el numero base y el segundo la potencia a elevar, regresando el resultado de la potencia.
+
+
+/*:
+### Enumeraciones
+A) Crea la enumaración "meses" para definir tipos de datos basados en los meses del año.
+*/
+
+
+//: B) Crear la función "numeroMes" que reciba el tipo de dato "meses" y regrese el numero del mes correspondiente
+
+
+
+//: C) Para regresar el numero de mes correspondiente utilizar la "switch"
+
+
+
+
