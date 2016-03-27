08667478f1db5d30f9f249c7c6736c10bf8e012b
Carlos Suárez <smca310777@gmail.com>
2016-03-26 18:09:09 -0600

    Actualización de actividades

--- actividad3/TM_A_03.playground/Contents.swift
+++ actividad3/TM_A_03.playground/Contents.swift
@@ -4,9 +4,6 @@ import UIKit
 * Tipos de datos
 * Asociación de tipos
 * Arreglos y Diccionarios
-* Condicionales y Ciclos
-* Funciones
-* Enumareción
 */
 
 
@@ -45,27 +42,6 @@ A) Declarar la variable "datos" con los valores [3,6,9,2,4,1]
 
 //: C) Encontrar los valores menores a 5
 
-
-/*:
-### Funciones
-A) Crea la función "suma" que reciba dos parámetros de tipo entero regresando la suma de ambos números.
-*/
-
-
-//: B) Crear la función "potencia" que reciba dos parámetros de tipo entero, el primer parámetro para el numero base y el segundo la potencia a elevar, regresando el resultado de la potencia.
-
-
-/*:
-### Enumeraciones
-A) Crea la enumaración "meses" para definir tipos de datos basados en los meses del año.
-*/
-
-
-//: B) Crear la función "numeroMes" que reciba el tipo de dato "meses" y regrese el numero del mes correspondiente
-
-
-
-//: C) Para regresar el numero de mes correspondiente utilizar la "switch"
 
 
 
